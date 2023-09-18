9b775e9542f4abb98a49471d3af485bff8b61f40
Yoshihiro Kameda <kameda.sbng@gmail.com>
2023-09-18 20:59:27 +0900

    add json data

---
+++ README.md
@@ -0,0 +1,9 @@
+# English-Japanese dictionary json files.
+
+This repository is based on https://github.com/kujirahand/EJDict
+
+## License
+
+- [Public Domain CC0](https://creativecommons.org/publicdomain/zero/1.0/)
+- [パブリックドメイン CC0](https://creativecommons.org/publicdomain/zero/1.0/deed.ja)
+
---
+++ convert_txt_to_json.rb
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+require 'json'
+
+def main
+  ('a'..'z').each {|c|
+    File.readlines("./src/#{c}.txt").each {|line|
+      word, translated = line.split("\t")
+      if (target_word?(word))
+        h = {
+          'word' => word,
+          'translated' => translated
+        }
+
+        open("./json/#{word}.json", "w") {|of|
+          of.write(h.to_json)
+        }
+      end
+    }
+  }
+end
+
+def target_word?(word)
+  if (word.include?("'"))
+    return false
+  end
+
+  if (word.include?("/"))
+    return false
+  end
+
+  return true
+end
+
+case $PROGRAM_NAME
+when __FILE__
+  main
+when /spec[^\/]*$/
+  # {spec of the implementation}
+end
+
